dd133dbbc0f02a28303fe6330ee14fe213c38255
Nikita Naleikin <nnaleikin361@gmail.com>
2024-02-09 19:13:11 +0400

    Replaces the `useComposedRefObject` and `useComposedRefCallback` hooks with `useComposedRef` hook

--- src/hooks/useComposedRef.ts
+++ src/hooks/useComposedRef.ts
@@ -1,31 +1,28 @@
 import * as React from 'react';
 
-function setRefs<T>(refs: React.Ref<T>[], value: T) {
-  for (const ref of refs)
-    if (typeof ref === 'function') ref(value);
-    // eslint-disable-next-line no-param-reassign
-    else if (ref) (ref as React.MutableRefObject<T>).current = value;
-}
+export function useComposedRef<T>(...refs: React.Ref<T>[]) {
+  const nonNullableRefs = refs.filter(Boolean) as NonNullable<React.Ref<T>>[];
 
-export function useComposedRefCallback<T>(
-  ...refs: React.Ref<T>[]
-): React.RefCallback<T> {
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  return React.useCallback((value) => setRefs(refs, value), refs);
-}
+  return React.useMemo(() => {
+    const composedRef = React.createRef<T>();
+    const nonNullableRefsLength = nonNullableRefs.length;
 
-export function useComposedRefObject<T>(
-  ...refs: React.Ref<T>[]
-): React.RefObject<T> {
-  return React.useMemo(
-    () =>
-      new Proxy(React.createRef(), {
-        set(...args) {
-          setRefs(refs, args[2]);
-          return Reflect.set(...args);
-        },
-      }),
+    if (nonNullableRefsLength === 0) return composedRef;
+
+    if (nonNullableRefsLength === 1 && typeof nonNullableRefs[0] === 'object')
+      return nonNullableRefs[0];
+
+    return new Proxy(composedRef, {
+      set(...args) {
+        const value = args[2] as T;
+
+        for (const ref of nonNullableRefs)
+          if (typeof ref === 'function') ref(value);
+          else (ref as React.MutableRefObject<T>).current = value;
+
+        return Reflect.set(...args);
+      },
+    });
     // eslint-disable-next-line react-hooks/exhaustive-deps
-    refs,
-  );
+  }, nonNullableRefs);
 }
